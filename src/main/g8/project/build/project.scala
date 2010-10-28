83002e2cf3e1f89a035b360fb4031cd4c3b04612
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 13:45:02 -0400

    Missing with keyword, and fix version of jetty

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class MyProject(info: ProjectInfo) extends DefaultWebProject(info) assembly.AssemblyBuilder {
+class MyProject(info: ProjectInfo) extends DefaultWebProject(info) with assembly.AssemblyBuilder {
   // repository locations (for Jersey)
   val javaNet = "Java.net Repository for Maven" at "http://download.java.net/maven/2/"
   val codaRepo = "Coda Hale's Repository" at "http://repo.codahale.com/"
@@ -14,7 +14,7 @@ class MyProject(info: ProjectInfo) extends DefaultWebProject(info) assembly.Asse
   val simpleVelocity = "bryanjswift" % "simple-velocity" % "0.3.0" from "http://github.com/downloads/bryanjswift/simple-velocity/simple-velocity_2.8.0-0.3.0.jar"
 
   // Jetty
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.26" % "test->default"
+  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.25" % "test->default"
   // ScalaTest
   val scalaTest = "org.scalatest" % "scalatest" % "1.2" % "test->default"
 
