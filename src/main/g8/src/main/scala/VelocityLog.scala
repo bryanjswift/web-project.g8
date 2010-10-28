139ccc90995d9cbedca6e8577667cf6849115c3f
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 13:48:20 -0400

    Back to java.util.logging

--- src/main/g8/src/main/scala/VelocityLog.scala
+++ src/main/g8/src/main/scala/VelocityLog.scala
@@ -1,23 +1,23 @@
 package $organization$
 
-import net.lag.logging.Logger
+import java.util.logging.{Level,Logger}
 import org.apache.velocity.runtime.{RuntimeServices => Services}
 import org.apache.velocity.runtime.log.LogChute
 
 class VelocityLog extends LogChute {
-  private val logger = Logger.get
+  private val logger = Logger.getLogger(getClass().getName())
   def init(services:Services) = { }
   def isLevelEnabled(level:Int) = logger.isLoggable(level)
   def log(level:Int, message:String) = logger.log(level,message)
-  def log(level:Int, message:String, t:Throwable) = logger.log(level,t,message)
+  def log(level:Int, message:String, t:Throwable) = logger.log(level,message,t)
   private implicit def intToLevel(l:Int):Level = {
     l match {
-      case LogChute.ERROR_ID => Logger.ERROR
-      case LogChute.WARN_ID => Logger.WARNING
-      case LogChute.INFO_ID => Logger.INFO
-      case LogChute.DEBUG_ID => Logger.DEBUG
-      case LogChute.TRACE_ID => Logger.TRACE
-      case _ => Logger.ALL
+      case LogChute.ERROR_ID => Level.SEVERE
+      case LogChute.WARN_ID => Level.WARNING
+      case LogChute.INFO_ID => Level.INFO
+      case LogChute.DEBUG_ID => Level.CONFIG
+      case LogChute.TRACE_ID => Level.FINE
+      case _ => Level.FINEST
     }
   }
 }
