c5967c87cb875e43ce9b6898a044d01f1e029412
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 16:06:05 -0400

    Add a velocity usage example

--- src/main/g8/src/main/scala/Index.scala
+++ src/main/g8/src/main/scala/Index.scala
@@ -13,4 +13,12 @@ class IndexResource {
     val xml = <h1>Hello $author$, From Jersey</h1>
     xml.toString
   }
+  @GET
+  @Path("/velocity")
+  @Produces(Array(TEXT_HTML))
+  def velocity:String = {
+    val context = Map[String,Any]()
+    val view = new VelocityView("templates/index.vm")
+    view.render(context)
+  }
 }
