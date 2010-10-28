19f34da3f858d5e6fa500831c6662f59882f0a02
Bryan J Swift <bryan.j.swift@gmail.com>
2010-10-28 10:59:05 -0400

    simple-velocity 0.3.0 from github download

--- src/main/g8/project/build/project.scala
+++ src/main/g8/project/build/project.scala
@@ -3,14 +3,12 @@ import sbt._
 class MyProject(info: ProjectInfo) extends DefaultWebProject(info) assembly.AssemblyBuilder {
   // repository locations (for Jersey)
   val javaNet = "Java.net Repository for Maven" at "http://download.java.net/maven/2/"
-  // Repository location for simple-velocity
-  val bryanjswift = "Bryan J Swift" at "http://repos.bryanjswift.com/maven2/"
 
   // Jersey
   val jersey = "com.sun.jersey" % "jersey-server" % "1.1.2-ea"
   // Velocity
   val velocity = "org.apache.velocity" % "velocity" % "1.6.2"
-  val simpleVelocity = "bryanjswift" % "simple-velocity" % "0.2.1"
+  val simpleVelocity = "bryanjswift" % "simple-velocity" % "0.3.0" from "http://github.com/downloads/bryanjswift/simple-velocity/simple-velocity_2.8.0-0.3.0.jar"
 
   // Jetty
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"
